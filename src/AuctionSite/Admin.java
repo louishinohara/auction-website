8c9e6d8836d0a9b6c6ecf0714c5f2ead3803d256
Isaac Rafael Perez <51679048+Zaplox40@users.noreply.github.com>
2021-04-21 03:25:37 -0400

    Update Admin.java

--- src/AuctionSite/Admin.java
+++ src/AuctionSite/Admin.java
@@ -2,10 +2,31 @@ package AuctionSite;
 
 public class Admin extends Person {
     private int adminID;
+    private double totalEarnings;
+    private double totalBike;
+    private double totalCar;
+    private double 
 
     public Admin(String firstName, String lastName, String SSN, String email, String address, int adminID){
         super( firstName,  lastName,  SSN,  email, address);
         this.adminID = adminID;
+        this.totalEarnings = 0;
     
     }
+    
+    private createCustomerRep(String firstName, String lastName, String SSN, String email, String address, int customerRepID)
+    {
+        CustomerRep employee = new CustomerRep(firstName,lastName,SSN, email, address,customerRepID);
+        return employee;
+    }
+    
+    private void findTotalEarnings()
+    {
+       return this.totalEarnings;
+    }
+
+    
+    
+    
+                                 
 }
